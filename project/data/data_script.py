98e3735d2e929d3ec0bf5ad651586e44558ce272
Fabian Westphal <fabian.westphal@fau.de>
2023-05-17 14:02:21 +0200

    added sqlite functionality

--- project/data/data_script.py
+++ project/data/data_script.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import os.path
-from urllib.parse import urlparse
+from sqlalchemy import create_engine
 
 
 def read_cycling_data():
@@ -14,7 +13,7 @@ def read_cycling_data():
     df = pd.read_csv(query_url)
     
     # 30/31 days * 24 hours * 4 15-min intervals
-    print(len(df), "entries") 
+    #print(len(df), "entries") 
     
     return df
 
@@ -22,18 +21,25 @@ def read_rain_data():
     stations = {"01766": "Münster/Osnabrück"}
     station = "01766"
     
+    # Download from https://opendata.dwd.de/climate_environment/CDC/observations_germany/climate/10_minutes/precipitation/historical/
+    # unpack and store .txt in /data folder
     file_path = f"./produkt_zehn_min_rr_20200101_20221231_{station}.txt"
     df = pd.read_csv(file_path, delimiter=";")
     
     # 3 years * 365 days * 24 hours * 6 10-min intervals + 1 leap day * 24 * 6
-    print(len(df), "entries")
+    #print(len(df), "entries")
     
     # convert time into correct format
     df["MESS_DATUM"] = pd.to_datetime(df["MESS_DATUM"], format="%Y%m%d%H%M")
     
     return df
 
+def write_data_to_sql(data, table_name):
+    engine = create_engine('sqlite:///my_data.sqlite', echo=False)
+    data.to_sql(table_name, con=engine, if_exists='replace')
+    
 df_cycling = read_cycling_data()
 df_rain = read_rain_data()    
-print(df_cycling.head())
-#print(df_rain["RWS_10"].value_counts())
+
+write_data_to_sql(df_cycling, "cycling")
+write_data_to_sql(df_rain, "rain")
